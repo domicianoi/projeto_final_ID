f21e565378ed1038b99e28ce8da225fc81579f5e
domicianoi <bellydomi78@gmail.com>
2025-10-21 18:11:22 -0300

    scripts

--- scripts/01_importacao/estrutura_pastas.R
+++ scripts/01_importacao/estrutura_pastas.R
@@ -1,4 +1,4 @@
 dirs <- c(
-  "dados/raw", "dados/processed", "scripts/01_importacao", "scripts/02_analise", "scripts/03_graficos", "resultados/figuras/mapa", "resultados/figuras/grafico_ggplot", "resultados/tabelas", "relatorio/"
-)
+  "dados/raw", "dados/processed", "scripts/01_importacao", "scripts/02_analise", "scripts/03_graficos", "resultados/figuras/mapa", "resultados/figuras/grafico_ggplot", "resultados/tabelas"
+  )
 for (d in dirs) if (!dir.exists(d)) dir.create (d, recursive = TRUE, showWarnings = FALSE)
